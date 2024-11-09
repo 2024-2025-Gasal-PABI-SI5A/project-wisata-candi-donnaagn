2ff33e552807d99c94336ea9742ddcd7a9072b67
donnaagn <161106016+donnaagn@users.noreply.github.com>
2024-11-09 10:52:21 +0700

    commit 2

---
+++ candi/lib/screens/detail_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:candi/models/candi.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
