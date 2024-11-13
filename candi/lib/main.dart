5e94440c1d0484a31846c095ce2733b6f982bdcf
donnaagn <161106016+donnaagn@users.noreply.github.com>
2024-11-13 23:25:21 +0700

    signin_screen

--- candi/lib/main.dart
+++ candi/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:candi/data/candi_data.dart';
+import 'package:candi/screens/signin_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:candi/screens/detail_screen.dart';
 import 'package:candi/screens/profile_screen.dart';
@@ -15,10 +16,22 @@ class MainApp extends StatelessWidget {
     return  MaterialApp(
       title: 'Wisata Candi',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        appBarTheme: const AppBarTheme(
+          iconTheme: IconThemeData(color: Colors.deepPurple),
+          titleTextStyle: TextStyle(
+            color: Colors.deepPurple,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple).copyWith(
+          primary: Colors.deepPurple,
+          surface: Colors.deepPurple[50],
+        ),
         useMaterial3: true
       ),
-      home: ProfileScreen(),
+      //home: ProfileScreen(),
+      home: SignInScreen(),
         );
       }
     }
